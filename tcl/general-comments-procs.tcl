83fe05922103219233515cfde20ead53bef30e4f
Jade Rubick <jade@rubick.com>
2005-06-27 15:28:06 +0000

    Apply patch 695 for bug 2516, increase standards compliance

--- tcl/general-comments-procs.tcl
+++ tcl/general-comments-procs.tcl
@@ -194,9 +194,9 @@ ad_proc -private general_comments_print_comment {
 
                 append attachments_html "<li>$title "
                 if { $mime_type == "image_gif" || $mime_type == "image/jpeg" } {
-                    append attachments_html "(<a href=\"${package_url}view-image?image_id=$item_id&return_url=$return_url\">$name</a>)\n"
+                    append attachments_html "(<a href=\"[ad_quotehtml ${package_url}view-image?image_id=$item_id&return_url=$return_url]\">$name</a>)\n"
                 } else {
-                    append attachments_html "(<a href=\"${package_url}file-download?item_id=$item_id\">$name</a>)\n"
+                    append attachments_html "(<a href=\"[ad_quotehtml ${package_url}file-download?item_id=$item_id]\">$name</a>)\n"
                 }
             }
 
@@ -204,15 +204,15 @@ ad_proc -private general_comments_print_comment {
 	              select i.item_id, e.label, e.url
 	                from cr_items i, cr_extlinks e
 	               where i.parent_id = :comment_id and e.extlink_id = i.item_id" {
-                append attachments_html "<li><a href=\"$url\">$label</a>\n"
+                append attachments_html "<li><a href=\"[ad_quotehtml $url]\">$label</a>\n"
             }
             if { ![empty_string_p $attachments_html] } {
                 append html "<h5>[_ general-comments.Attachments]</h5>\n<ul>\n$attachments_html</ul>\n"
             }
         }
-        append html "<p>-- <a href=\"/shared/community-member?user_id=$creation_user\">$author</a> [_ general-comments.on] $pretty_date2 (<a href=\"${package_url}view-comment?[export_url_vars comment_id return_url]\">[_ general-comments.view_details]</a>)</p>\n"
+        append html "<p>-- <a href=\"[ad_quotehtml /shared/community-member?user_id=$creation_user]\">$author</a> [_ general-comments.on] $pretty_date2 (<a href=\"[ad_quotehtml ${package_url}view-comment?[export_url_vars comment_id return_url]]\">[_ general-comments.view_details]</a>)</p>\n"
     } else {
-        append html "<li><a href=\"${package_url}view-comment?[export_url_vars comment_id return_url]\">$title</a> [_ general-comments.by] <a href=\"/shared/community-member?user_id=$creation_user\">$author</a> [_ general-comments.on] $pretty_date<br>\n"
+        append html "<li><a href=\"[ad_quotehtml ${package_url}view-comment?[export_url_vars comment_id return_url]]\">$title</a> [_ general-comments.by] <a href=\"[ad_quotehtml /shared/community-member?user_id=$creation_user]\">$author</a> [_ general-comments.on] $pretty_date<br>\n"
     }
 
     return $html
@@ -250,7 +250,7 @@ ad_proc -public general_comments_create_link {
     if { ![info exists object_name] } { set object_name [acs_object_name $object_id] }
     if { ![info exists context_id] } { set context_id $object_id }
 
-    set html "<a href=\"${package_url}comment-add?[export_url_vars object_id object_name return_url context_id category]\" $link_attributes>$link_text</a>"
+    set html "<a href=\"[ad_quotehtml ${package_url}comment-add?[export_url_vars object_id object_name return_url context_id category]]\" $link_attributes>$link_text</a>"
 
     return $html
 }
@@ -297,7 +297,7 @@ ad_proc -deprecated get_comments {object_id return_url} {
                     o.object_id = g.comment_id and
                     p.person_id = o.creation_user
               order by creation_date" {
-        append html "<li><a href=\"${package_url}view-comment?[export_url_vars comment_id return_url]\">$title</a> by $author, $creation_date<br>\n"
+        append html "<li><a href=\"[ad_quotehtml ${package_url}view-comment?[export_url_vars comment_id return_url]]\">$title</a> by $author, $creation_date<br>\n"
     }
     return "$html"
 }
@@ -315,7 +315,7 @@ ad_proc -deprecated create_link {object_id object_name return_url link_text {con
     # get the package url
     set package_url [general_comments_package_url]
 
-    set html "<a href=\"${package_url}comment-add?[export_url_vars object_id object_name return_url context_id category]\">$link_text</a>"
+    set html "<a href=\"[ad_quotehtml ${package_url}comment-add?[export_url_vars object_id object_name return_url context_id category]]\">$link_text</a>"
     return $html
 }
 
