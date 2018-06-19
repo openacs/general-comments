e2c67b4367adbb45ae503999d9ca7e0ca41f5f92
Hector Romojaro <hector.romojaro@gmail.com>
2018-06-19 13:45:23 +0000

    Whitespace changes

--- www/index.tcl
+++ www/index.tcl
@@ -2,12 +2,12 @@
 
 ad_page_contract {
     General comments main page
-    
+
     @author Phong Nguyen (phong@arsdigita.com)
     @author Pascal Scheffers (pascal@scheffers.net)
     @creation-date 2000-10-12
     @cvs-id $Id$
-} -query { 
+} -query {
     {orderby:token,optional}
     {approval "any"}
     {modified "any"}
@@ -51,34 +51,34 @@ template::list::create -name comments_list \
     -no_data "#general-comments.lt_No_comments_available#" \
     -html {style "margin: 0 auto"} \
     -elements {
-	counter {
-	    label "#general-comments.Num#"
+        counter {
+            label "#general-comments.Num#"
             display_template {@comments.rownum;literal@}
-	}
+        }
         comment_id {
-	    label "#general-comments.ID#"
-	    display_template {<a href="view-comment?comment_id=@comments.comment_id@">@comments.comment_id@</a>}
-	    orderby {comment_id}
-	}
-	title {
-	    label "#general-comments.Title_1#"
-	    orderby {title}
-	}
-	approved_p {
-	    label "#general-comments.Approved#"
-	    html {align center}
-	    orderby {approved_p}
-	}
-	live_version_p {
-	    label "#general-comments.Has_live_version#" 
-	    html {align center}
-	    orderby {approved_p}
-	}
-	pretty_date {
-	    label "#general-comments.Last_Modified#"
-	    orderby {creation_date}
-	} 
-    } -filters {approval {} modified {}} 
+            label "#general-comments.ID#"
+            display_template {<a href="view-comment?comment_id=@comments.comment_id@">@comments.comment_id@</a>}
+            orderby {comment_id}
+        }
+        title {
+            label "#general-comments.Title_1#"
+            orderby {title}
+        }
+        approved_p {
+            label "#general-comments.Approved#"
+            html {align center}
+            orderby {approved_p}
+        }
+        live_version_p {
+            label "#general-comments.Has_live_version#"
+            html {align center}
+            orderby {approved_p}
+        }
+        pretty_date {
+            label "#general-comments.Last_Modified#"
+            orderby {creation_date}
+        }
+} -filters {approval {} modified {}}
 
 set yes [_ acs-kernel.common_Yes]
 set no  [_ acs-kernel.common_No]
